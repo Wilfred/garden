2ed93f6aca5fb3bbda616cc3899f13f37f53d798
Wilfred Hughes <me@wilfred.me.uk>
2022-12-04 18:10:38 -0800

    Add textarea and slurp contents in JS

--- src/parse.js
+++ src/parse.js
@@ -1,3 +1,8 @@
 function lex(src) {
   return src.split(" ");
 }
+
+setInterval(() => {
+  const src = document.querySelectorAll("#editor")[0].value;
+  document.querySelectorAll("#result")[0].textContent = src;
+}, 1000);

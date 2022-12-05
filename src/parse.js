a53abc100f40b1800c83d90e83f1b1162cd18b9b
Wilfred Hughes <me@wilfred.me.uk>
2022-12-04 22:36:29 -0800

    UI polish

--- src/parse.js
+++ src/parse.js
@@ -2,7 +2,17 @@ function lex(src) {
   return src.split(" ");
 }
 
+function parse(tokens) {
+  if (tokens.length == 1 || tokens.length == 3) {
+    return tokens;
+  }
+
+  return tokens;
+}
+
 setInterval(() => {
   const src = document.querySelectorAll("#editor")[0].value;
-  document.querySelectorAll("#result")[0].textContent = src;
+  const ast = parse(lex(src));
+
+  document.querySelectorAll("#result")[0].textContent = JSON.stringify(ast);
 }, 1000);

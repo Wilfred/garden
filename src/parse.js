dcfb2ef7f09b4d50e6cb4c70d3c9596ea5effca0
Wilfred Hughes <me@wilfred.me.uk>
2022-12-04 22:43:56 -0800

    Lex and error reporting

--- src/parse.js
+++ src/parse.js
@@ -1,5 +1,6 @@
 function lex(src) {
-  return src.split(" ");
+  const tokenRegex = /[0-9]+|[+-]|;/g;
+  return Array.from(src.matchAll(tokenRegex), (m) => m[0]);
 }
 
 function parse(tokens) {
@@ -7,10 +8,21 @@ function parse(tokens) {
     return tokens;
   }
 
-  return tokens;
+  developerError("Wrong number of tokens: " + tokens.length);
+  return [];
+}
+
+function reset() {
+  document.querySelectorAll("#error")[0].textContent = "";
+}
+
+function developerError(msg) {
+  document.querySelectorAll("#error")[0].textContent = msg;
 }
 
 setInterval(() => {
+  reset();
+
   const src = document.querySelectorAll("#editor")[0].value;
   const ast = parse(lex(src));
 

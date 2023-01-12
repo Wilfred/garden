72828bde6df2f8e1d2d49c34d2e0bffdc252e7d2
Wilfred Hughes <me@wilfred.me.uk>
2023-01-11 22:01:09 -0800

    Read input in a loop

--- garden/src/main.rs
+++ garden/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut input = String::new();
+        match std::io::stdin().read_line(&mut input) {
+            Ok(n) => {
+                println!("{n} bytes read");
+                println!("{input}");
+            }
+            Err(error) => println!("error: {error}"),
+        }
+    }
 }

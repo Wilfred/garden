2e4d30ebcb0dd9df970ed95bbd3c2a0eb63a3efd
Wilfred Hughes <me@wilfred.me.uk>
2023-01-12 17:48:21 -0800

    Skeleton integer literal parse

--- garden/src/main.rs
+++ garden/src/main.rs
@@ -1,5 +1,18 @@
 use std::io::Write;
 
+#[derive(Debug)]
+enum Expression {
+    Integer(i64),
+}
+
+fn parse_integer(tokens: &[&str]) -> Result<Expression, String> {
+    if tokens.first().is_some() {
+        return Ok(Expression::Integer(1));
+    }
+
+    Ok(Expression::Integer(0))
+}
+
 fn lex(s: &str) -> Vec<&str> {
     s.split(' ').collect()
 }
@@ -13,7 +26,9 @@ fn main() {
         std::io::stdout().flush().unwrap();
         match std::io::stdin().read_line(&mut input) {
             Ok(_) => {
-                dbg!(lex(&input));
+                let tokens = lex(&input);
+                let ast = parse_integer(&tokens);
+                println!("{:?}", ast)
             }
             Err(error) => println!("error: {error}"),
         }

fc9e1fab0ec806096f254140226e54a3179ed9a0
Wilfred Hughes <me@wilfred.me.uk>
2023-01-12 17:40:30 -0800

    Skeleton lexer

--- garden/src/main.rs
+++ garden/src/main.rs
@@ -1,10 +1,13 @@
+fn lex(s: &str) -> Vec<&str> {
+    s.split(' ').collect()
+}
+
 fn main() {
     loop {
         let mut input = String::new();
         match std::io::stdin().read_line(&mut input) {
-            Ok(n) => {
-                println!("{n} bytes read");
-                println!("{input}");
+            Ok(_) => {
+                dbg!(lex(&input));
             }
             Err(error) => println!("error: {error}"),
         }

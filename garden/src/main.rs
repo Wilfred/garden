f917466d01d354791f079fb41886eb96fad93ef9
Wilfred Hughes <me@wilfred.me.uk>
2023-01-12 17:42:39 -0800

    Add a basic prompt UI

--- garden/src/main.rs
+++ garden/src/main.rs
@@ -1,10 +1,16 @@
+use std::io::Write;
+
 fn lex(s: &str) -> Vec<&str> {
     s.split(' ').collect()
 }
 
 fn main() {
+    println!("Welcome to the garden!");
+
     loop {
+        print!("> ");
         let mut input = String::new();
+        std::io::stdout().flush().unwrap();
         match std::io::stdin().read_line(&mut input) {
             Ok(_) => {
                 dbg!(lex(&input));

7f77280945ecd403bd9e232d397ed852ce0554f8
Wilfred Hughes <me@wilfred.me.uk>
2023-01-12 18:01:12 -0800

    Require a token when parsing

--- garden/src/main.rs
+++ garden/src/main.rs
@@ -5,15 +5,28 @@ enum Expression {
     Integer(i64),
 }
 
-fn parse_integer(tokens: &[&str]) -> Result<Expression, String> {
-    if tokens.first().is_some() {
-        return Ok(Expression::Integer(1));
+fn pop_token<'a, 'b>(tokens: &'a mut &[&'b str]) -> Option<&'b str> {
+    if tokens.is_empty() {
+        return None;
     }
 
-    Ok(Expression::Integer(0))
+    let token = tokens[0];
+    *tokens = &tokens[1..];
+    Some(token)
+}
+
+fn parse_integer(tokens: &mut &[&str]) -> Result<Expression, String> {
+    match pop_token(tokens) {
+        Some(_token) => Ok(Expression::Integer(1)),
+        None => Err("Expected integer, got EOF".into()),
+    }
 }
 
 fn lex(s: &str) -> Vec<&str> {
+    if s.is_empty() {
+        return vec![];
+    }
+
     s.split(' ').collect()
 }
 
@@ -26,8 +39,9 @@ fn main() {
         std::io::stdout().flush().unwrap();
         match std::io::stdin().read_line(&mut input) {
             Ok(_) => {
-                let tokens = lex(&input);
-                let ast = parse_integer(&tokens);
+                let tokens = lex(input.trim());
+                let mut token_ptr = &tokens[..];
+                let ast = parse_integer(&mut token_ptr);
                 println!("{:?}", ast)
             }
             Err(error) => println!("error: {error}"),

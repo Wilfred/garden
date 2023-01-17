4ed16e059cd2192bbb9be450e756c30fb6e42772
Wilfred Hughes <me@wilfred.me.uk>
2023-01-17 09:01:04 -0800

    Basic environment

--- garden/src/main.rs
+++ garden/src/main.rs
@@ -1,11 +1,11 @@
 mod parse;
 
-use std::{fmt::Display, io::Write};
+use std::{collections::HashMap, fmt::Display, io::Write};
 
 use crate::parse::{lex, parse_toplevel, Expression};
 use owo_colors::OwoColorize;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Value {
     Integer(i64),
     Boolean(bool),
@@ -20,20 +20,20 @@ impl Display for Value {
     }
 }
 
-fn evaluate(expr: &Expression) -> Result<Value, String> {
+fn evaluate(expr: &Expression, env: &HashMap<String, Value>) -> Result<Value, String> {
     match expr {
         Expression::Integer(i) => Ok(Value::Integer(*i)),
         Expression::Boolean(b) => Ok(Value::Boolean(*b)),
         Expression::BinaryOperator(lhs, _, rhs) => {
-            let lhs_value = evaluate(lhs)?;
-            let rhs_value = evaluate(rhs)?;
+            let lhs_value = evaluate(lhs, env)?;
+            let rhs_value = evaluate(rhs, env)?;
 
             let lhs_num = match lhs_value {
                 Value::Integer(i) => i,
                 _ => {
                     let lhs_new =
                         read_replacement(&format!("Expected an integer, but got: {}", lhs_value))?;
-                    match evaluate(&lhs_new)? {
+                    match evaluate(&lhs_new, env)? {
                         Value::Integer(i) => i,
                         v => {
                             return Err(format!("Expected an integer, but got: {}", v));
@@ -47,7 +47,7 @@ fn evaluate(expr: &Expression) -> Result<Value, String> {
                 _ => {
                     let rhs_new =
                         read_replacement(&format!("Expected an integer, but got: {}", rhs_value))?;
-                    match evaluate(&rhs_new)? {
+                    match evaluate(&rhs_new, env)? {
                         Value::Integer(i) => i,
                         v => {
                             return Err(format!("Expected an integer, but got: {}", v));
@@ -58,10 +58,13 @@ fn evaluate(expr: &Expression) -> Result<Value, String> {
 
             Ok(Value::Integer(lhs_num + rhs_num))
         }
-        Expression::Variable(s) => {
-            let expr_new = read_replacement(&format!("Unbound variable: {}", s))?;
-            evaluate(&expr_new)
-        }
+        Expression::Variable(s) => match env.get(s) {
+            Some(v) => Ok(v.clone()),
+            None => {
+                let expr_new = read_replacement(&format!("Unbound variable: {}", s))?;
+                evaluate(&expr_new, env)
+            }
+        },
     }
 }
 
@@ -96,6 +99,9 @@ fn main() {
         "!".bold()
     );
 
+    let mut env: HashMap<String, Value> = HashMap::new();
+    env.insert("x".into(), Value::Boolean(true));
+
     loop {
         println!();
         prompt_symbol(0);
@@ -107,7 +113,7 @@ fn main() {
                 let mut token_ptr = &tokens[..];
 
                 match parse_toplevel(&mut token_ptr) {
-                    Ok(expr) => match evaluate(&expr) {
+                    Ok(expr) => match evaluate(&expr, &env) {
                         Ok(result) => {
                             println!("{}", result)
                         }

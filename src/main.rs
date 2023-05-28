0c41531033a62062dd41d2af1dd5da3667c4ebb4
Wilfred Hughes <me@wilfred.me.uk>
2023-05-28 10:41:32 -0700

    Highlight the bad line and position on parse errors during run

--- src/main.rs
+++ src/main.rs
@@ -54,6 +54,26 @@ fn main() {
     }
 }
 
+fn line_of_offset<'a>(src: &'a str, byte_offset: usize) -> (&'a str, usize, usize) {
+    let mut line_start_offset = 0;
+
+    for (i, line) in src.lines().enumerate() {
+        if line.len() + line_start_offset >= byte_offset {
+            return (line, i, byte_offset - line_start_offset);
+        }
+
+        // TODO: This is wrong if src contains \r\n newlines.
+        line_start_offset += line.len() + 1;
+    }
+
+    let last_line = match src.lines().last() {
+        Some(line) => line,
+        None => src, // empty string
+    };
+
+    (last_line, 0, last_line.len())
+}
+
 fn run_file(src_bytes: Vec<u8>, path: &PathBuf, interrupted: &Arc<AtomicBool>) {
     match String::from_utf8(src_bytes) {
         Ok(src) => match parse_def_or_expr_from_str(path, &src) {
@@ -97,7 +117,15 @@ fn run_file(src_bytes: Vec<u8>, path: &PathBuf, interrupted: &Arc<AtomicBool>) {
             }
             Err(parse::ParseError::OtherError(pos, e)) => {
                 eprintln!("--> {}", pos.1.display());
-                eprintln!("Parse error: {}", e);
+
+                let (line_src, line_i, line_offset) = line_of_offset(&src, pos.0);
+                let formatted_line_num = format!("{} | ", line_i + 1);
+                eprintln!("{}{}", formatted_line_num, line_src);
+
+                let caret_space = " ".repeat(formatted_line_num.len() + line_offset);
+                eprintln!("{}^", caret_space);
+
+                eprintln!("\nParse error: {}", e);
             }
             Err(parse::ParseError::Incomplete(e)) => {
                 eprintln!("Parse error (incomplete input): {}", e);

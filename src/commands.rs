e3c02da1cbad05df8033344f55bf531d40d32004
Wilfred Hughes <me@wilfred.me.uk>
2023-10-09 08:28:46 -0700

    Pretty-print in :fstmts

--- src/commands.rs
+++ src/commands.rs
@@ -418,7 +418,7 @@ pub fn run_command<T: Write>(
         Command::FrameStatements => {
             if let Some(stack_frame) = env.stack.last() {
                 for (_, expr) in stack_frame.exprs_to_eval.iter().rev() {
-                    writeln!(buf, "{:?}", expr.1).unwrap();
+                    writeln!(buf, "{:#?}", expr.1).unwrap();
                 }
             }
         }

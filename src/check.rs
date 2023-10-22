10b831b8f97838de76219a9f2a71571acbd2b5fc
Wilfred Hughes <me@wilfred.me.uk>
2023-10-22 11:15:00 -0700

    check: Include error message in a JSONL format

--- src/check.rs
+++ src/check.rs
@@ -6,27 +6,36 @@ use crate::parse::{self, parse_toplevel_items};
 #[derive(Debug, Serialize)]
 struct CheckDiagnostic {
     line_number: usize,
+    message: String,
 }
 
 pub fn check(path: &Path, src: &str) {
     let errors = match parse_toplevel_items(path, src) {
         Ok(_) => vec![],
         Err(e) => match e {
-            parse::ParseError::Invalid { position, .. } => {
+            parse::ParseError::Invalid {
+                position, message, ..
+            } => {
                 // Expose line numbers as 1-indexed.
                 vec![CheckDiagnostic {
                     line_number: position.line_number + 1,
+                    message: message.0,
                 }]
             }
-            parse::ParseError::Incomplete(_) => {
+            parse::ParseError::Incomplete(message) => {
                 // TODO: last line would be better?
-                vec![CheckDiagnostic { line_number: 1 }]
+                vec![CheckDiagnostic {
+                    line_number: 1,
+                    message: message.0,
+                }]
             }
         },
     };
 
-    let error_json = serde_json::to_string(&errors).expect("TODO: can this ever fail?");
-    println!("{}", error_json);
+    for error in &errors {
+        println!("{}", serde_json::to_string(error).expect("TODO: can this ever fail?"));
+    }
+    
     if !errors.is_empty() {
         std::process::exit(1);
     }

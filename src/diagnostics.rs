4c533a9d047a0495eb55bb87b2da09f02bdd9597
Wilfred Hughes <me@wilfred.me.uk>
2023-10-27 22:03:15 -0700

    Ensure we have SourceString even when errors occur in tests

--- src/diagnostics.rs
+++ src/diagnostics.rs
@@ -20,13 +20,9 @@ pub fn format_error_with_stack(
     // For the topmost (most recently called) stack frame, the
     // relevant position is where the error occurred.
     let top_stack = stack.last().unwrap();
-    let src_string = top_stack
-        .enclosing_fun
-        .as_ref()
-        .map(|fi| fi.src_string.clone());
     res.push_str(&format_pos_in_fun(
         position,
-        src_string.as_ref(),
+        &top_stack.src,
         &top_stack.enclosing_name,
     ));
 
@@ -36,7 +32,7 @@ pub fn format_error_with_stack(
             res.push('\n');
             res.push_str(&format_pos_in_fun(
                 pos,
-                Some(&caller_stack_frame.src),
+                &caller_stack_frame.src,
                 &caller_stack_frame.enclosing_name,
             ));
         }
@@ -65,11 +61,7 @@ pub fn format_parse_error(message: &ErrorMessage, position: &Position, src: &str
     format_error(message, position, src)
 }
 
-fn format_pos_in_fun(
-    position: &Position,
-    src_string: Option<&SourceString>,
-    name: &SymbolName,
-) -> String {
+fn format_pos_in_fun(position: &Position, src_string: &SourceString, name: &SymbolName) -> String {
     let mut res = String::new();
 
     res.push_str(
@@ -83,32 +75,27 @@ fn format_pos_in_fun(
         .to_string(),
     );
 
-    let relevant_line = match src_string {
-        Some(src_string) => {
-            let src = &src_string.src;
-            let line_positions = LinePositions::from(src.as_str());
+    let src = &src_string.src;
+    let line_positions = LinePositions::from(src.as_str());
 
-            // TODO: this is the line number relative to the start of
-            // the SourceString, not the start of the file.
-            let offset = std::cmp::max(
-                position.start_offset as isize - src_string.offset as isize,
-                0,
-            ) as usize;
-            let line_num = if offset >= src.len() {
-                // TODO: this occurs when we are using the wrong
-                // SourceString, such as the main function wrapper. We
-                // should find the relevant SourceString instead.
-                0.into()
-            } else {
-                line_positions.from_offset(offset)
-            };
-
-            let s_lines: Vec<_> = src.lines().collect();
-            s_lines[line_num.as_usize()].to_owned()
-        }
-        None => "??? no source found (FunInfo is None) ???".to_owned(),
+    // TODO: this is the line number relative to the start of
+    // the SourceString, not the start of the file.
+    let offset = std::cmp::max(
+        position.start_offset as isize - src_string.offset as isize,
+        0,
+    ) as usize;
+    let line_num = if offset >= src.len() {
+        // TODO: this occurs when we are using the wrong
+        // SourceString, such as the main function wrapper. We
+        // should find the relevant SourceString instead.
+        0.into()
+    } else {
+        line_positions.from_offset(offset)
     };
 
+    let s_lines: Vec<_> = src.lines().collect();
+    let relevant_line = s_lines[line_num.as_usize()].to_owned();
     res.push_str(&relevant_line);
+
     res
 }

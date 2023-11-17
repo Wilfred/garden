1cf6d6e480afe3782f5bce88485f97d11b290753
Wilfred Hughes <me@wilfred.me.uk>
2023-11-16 23:44:16 -0800

    Warn on non-existent types on params too

--- src/eval.rs
+++ src/eval.rs
@@ -297,6 +297,16 @@ pub fn push_test_stackframe(test: &TestInfo, env: &mut Env) {
 fn check_types_exist(fun_info: &FunInfo, env: &Env) -> Vec<Warning> {
     let mut warnings = vec![];
 
+    for param in &fun_info.params {
+        if let Some(return_type) = &param.type_ {
+            if !env.types.contains_key(return_type) {
+                warnings.push(Warning {
+                    message: format!("No such type: {return_type}"),
+                });
+            }
+        }
+    }
+
     if let Some(return_type) = &fun_info.return_type {
         if !env.types.contains_key(return_type) {
             warnings.push(Warning {

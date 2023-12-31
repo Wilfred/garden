c79b209f67a0031ebf6bafafaf46d9a288003b20
Wilfred Hughes <me@wilfred.me.uk>
2023-12-30 23:25:47 -0800

    Define a basic garden_lang_web crate

---
+++ garden_lang_web/src/lib.rs
@@ -0,0 +1,8 @@
+use wasm_bindgen::prelude::*;
+
+// https://rustwasm.github.io/docs/book/game-of-life/hello-world.html#build-the-project
+
+#[wasm_bindgen]
+pub fn check_parse(s: &str) -> bool {
+    true
+}

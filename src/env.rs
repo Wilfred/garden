ab4e90f7978d789a2c717788a7a08039ab91ba43
Wilfred Hughes <me@wilfred.me.uk>
2024-04-06 15:35:37 -0700

    Fix path reported for builtins

--- src/env.rs
+++ src/env.rs
@@ -249,10 +249,10 @@ impl Default for Env {
             .expect("Prelude should be syntactically legal");
         eval_toplevel_defs(&prelude_items, &mut env);
 
-        let prelude_src = include_str!("builtins.gdn");
-        let prelude_items = parse_toplevel_items(&PathBuf::from("prelude.gdn"), prelude_src)
+        let builtins_src = include_str!("builtins.gdn");
+        let builtin_items = parse_toplevel_items(&PathBuf::from("builtins.gdn"), builtins_src)
             .expect("Stubs for built-ins should be syntactically legal");
-        eval_toplevel_defs(&prelude_items, &mut env);
+        eval_toplevel_defs(&builtin_items, &mut env);
 
         env
     }

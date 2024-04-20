9c4dffbf03a2240783819cac7864c4ba359b99ea
Wilfred Hughes <me@wilfred.me.uk>
2024-04-20 14:51:48 -0700

    All checks use toplevel items

--- src/checks.rs
+++ src/checks.rs
@@ -6,7 +6,7 @@ mod type_checker;
 use crate::diagnostics::Warning;
 use crate::env::Env;
 use crate::eval::eval_defs;
-use garden_lang_parser::ast::{Definition, Definition_, FunInfo, MethodKind, ToplevelItem};
+use garden_lang_parser::ast::{Definition, ToplevelItem};
 
 use self::hints::check_hints;
 use self::type_checker::check_types;
@@ -32,41 +32,6 @@ pub(crate) fn check_toplevel_items(items: &[ToplevelItem]) -> Vec<Warning> {
     warnings.extend(check_struct_fields(items, &env));
     warnings.extend(check_hints(items, &env));
     warnings.extend(check_types(items));
-
-    for def in &definitions {
-        warnings.extend(check_def(def));
-    }
-
-    warnings
-}
-
-pub(crate) fn check_def(def: &Definition) -> Vec<Warning> {
-    match &def.2 {
-        Definition_::Fun(_, fun_info) => check_fun_info(fun_info),
-        Definition_::Method(meth_info) => {
-            let fun_info = match &meth_info.kind {
-                MethodKind::BuiltinMethod(_, fun_info) => fun_info.as_ref(),
-                MethodKind::UserDefinedMethod(fun_info) => Some(fun_info),
-            };
-
-            let mut warnings = vec![];
-
-            if let Some(fun_info) = fun_info {
-                warnings.extend(check_fun_info(fun_info));
-            }
-
-            warnings
-        }
-        Definition_::Test(_) => vec![],
-        Definition_::Enum(_) => vec![],
-        Definition_::Struct(_) => vec![],
-    }
-}
-
-fn check_fun_info(fun_info: &FunInfo) -> Vec<Warning> {
-    let mut warnings = vec![];
-
-    // warnings.extend(check_types(fun_info));
 
     warnings
 }

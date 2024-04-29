4002c0eb7744894876cd0f1f760e222b94de07c3
Wilfred Hughes <me@wilfred.me.uk>
2024-04-29 08:44:19 -0700

    Emit diagnostic on duplicate methods

--- src/checks/duplicates.rs
+++ src/checks/duplicates.rs
@@ -1,6 +1,12 @@
+//! Check for duplicate definitions within a file. It's fine to
+//! redefine a definition that's currently in the environment, that's
+//! normal program development. Repeated definitions in a file is a
+//! mistake however.
+
+use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 
-use garden_lang_parser::ast::{Definition, Definition_, SymbolName, ToplevelItem};
+use garden_lang_parser::ast::{Definition, Definition_, SymbolName, ToplevelItem, TypeName};
 use garden_lang_parser::position::Position;
 
 use crate::diagnostics::Level;
@@ -9,6 +15,7 @@ use crate::{diagnostics::Diagnostic, env::Env};
 
 struct DuplicatesVisitor {
     funs_seen: HashMap<SymbolName, Position>,
+    methods_seen: HashMap<TypeName, HashMap<SymbolName, Position>>,
     warnings: Vec<Diagnostic>,
 }
 
@@ -30,7 +37,34 @@ impl Visitor for DuplicatesVisitor {
                         .insert(sym.name.clone(), sym.position.clone());
                 }
             }
-            Definition_::Method(_) => {}
+            Definition_::Method(method_info) => {
+                let meth_sym = &method_info.name_sym;
+
+                let mut is_repeat = false;
+                let type_name = &method_info.receiver_hint.sym.name;
+                match self.methods_seen.entry(type_name.clone()) {
+                    Entry::Occupied(occupied) => {
+                        is_repeat = occupied.get().contains_key(&meth_sym.name);
+                    }
+                    Entry::Vacant(vacant) => {
+                        let mut items = HashMap::default();
+                        items.insert(meth_sym.name.clone(), meth_sym.position.clone());
+
+                        vacant.insert(items);
+                    }
+                }
+
+                if is_repeat {
+                    self.warnings.push(Diagnostic {
+                        message: format!(
+                            "The method `{}::{}` is already defined in this file.",
+                            type_name, meth_sym.name
+                        ),
+                        position: meth_sym.position.clone(),
+                        level: Level::Warning,
+                    });
+                }
+            }
             Definition_::Test(_) => {}
             Definition_::Enum(_) => {}
             Definition_::Struct(_) => {}
@@ -44,6 +78,7 @@ pub(crate) fn check_duplicates(items: &[ToplevelItem], _env: &Env) -> Vec<Diagno
     let mut visitor = DuplicatesVisitor {
         warnings: vec![],
         funs_seen: HashMap::default(),
+        methods_seen: HashMap::default(),
     };
     for item in items {
         visitor.visit_toplevel_item(item);

3cc94570a0fc59d1b431dd321d232b8bef92620e
Wilfred Hughes <me@wilfred.me.uk>
2024-05-16 01:36:47 +0100

    Add helper function for finding the type parameters of a typedef

--- src/types.rs
+++ src/types.rs
@@ -1,10 +1,12 @@
-use garden_lang_parser::ast::{EnumInfo, StructInfo};
+use garden_lang_parser::{
+    ast::{EnumInfo, StructInfo, TypeSymbol},
+    position::Position,
+};
 
 #[derive(Debug, Clone, PartialEq)]
 pub(crate) enum BuiltinType {
     Int,
     String,
-    // TODO: these require a type parameter.
     Fun,
     List,
 }
@@ -14,4 +16,31 @@ pub(crate) enum TypeDef {
     Builtin(BuiltinType),
     Enum(EnumInfo),
     Struct(StructInfo),
+}
+
+impl TypeDef {
+    fn params(&self) -> Vec<TypeSymbol> {
+        match self {
+            TypeDef::Builtin(built_in_type) => match built_in_type {
+                BuiltinType::Int => vec![],
+                BuiltinType::String => vec![],
+                BuiltinType::Fun => vec![
+                    TypeSymbol {
+                        name: "Args".into(),
+                        position: Position::todo(),
+                    },
+                    TypeSymbol {
+                        name: "Ret".into(),
+                        position: Position::todo(),
+                    },
+                ],
+                BuiltinType::List => vec![TypeSymbol {
+                    name: "T".into(),
+                    position: Position::todo(),
+                }],
+            },
+            TypeDef::Enum(enum_info) => enum_info.type_params.clone(),
+            TypeDef::Struct(struct_info) => struct_info.type_params.clone(),
+        }
+    }
 }

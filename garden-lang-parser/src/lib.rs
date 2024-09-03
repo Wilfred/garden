c86c439daf44ac68785b1fecae8ac535d52ecf3f
Wilfred Hughes <me@wilfred.me.uk>
2024-09-02 23:42:01 -0700

    Ban remaining semicolon usage

--- garden-lang-parser/src/lib.rs
+++ garden-lang-parser/src/lib.rs
@@ -811,12 +811,7 @@ fn parse_general_expression(
         }
     }
 
-    let expr = parse_simple_expression_or_binop(src, tokens, id_gen, diagnostics);
-    if !is_inline && peeked_symbol_is(tokens, ";") {
-        tokens.pop();
-    }
-
-    expr
+    parse_simple_expression_or_binop(src, tokens, id_gen, diagnostics)
 }
 
 /// In Garden, an expression can only contain a single binary

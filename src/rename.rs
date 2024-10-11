9a15449d3af1e5ddb4cf3923e391aecac948f1d3
Wilfred Hughes <me@wilfred.me.uk>
2024-10-10 23:02:40 -0700

    Basic symbol visitor for rename

--- src/rename.rs
+++ src/rename.rs
@@ -1,12 +1,14 @@
-use std::path::Path;
+use std::{collections::HashMap, path::Path};
 
-use garden_lang_parser::{ast::Expression_, parse_toplevel_items, visitor::Visitor};
+use garden_lang_parser::{
+    ast::{Symbol, SyntaxId},
+    parse_toplevel_items,
+    position::Position,
+    visitor::Visitor,
+};
 
 use crate::{
-    checks::type_checker::check_types,
-    env::Env,
-    eval::eval_toplevel_defs,
-    pos_to_id::{find_expr_of_id, find_item_at},
+    checks::type_checker::check_types, env::Env, eval::eval_toplevel_defs, pos_to_id::find_item_at,
 };
 
 pub(crate) fn rename(src: &str, path: &Path, offset: usize, new_name: &str) {
@@ -27,19 +29,34 @@ pub(crate) fn rename(src: &str, path: &Path, offset: usize, new_name: &str) {
         return;
     };
 
-    let Some(expr) = find_expr_of_id(&items, *id) else {
-        eprintln!("No expression found for id {:?}", id);
-        return;
+    let mut visitor = RenameLocalVisitor {
+        definition_pos: def_pos.clone(),
+        id_to_pos: id_to_pos.clone(),
+        replace_positions: vec![],
     };
 
-    match &expr.expr_ {
-        Expression_::Variable(symbol) => todo!(),
-        _ => {}
+    for item in items {
+        visitor.visit_toplevel_item(&item);
     }
 
-    print!("{}", src);
+    print!("{:?}", visitor.replace_positions);
 }
 
-struct RenameLocalVisitor {}
+struct RenameLocalVisitor {
+    definition_pos: Position,
+    id_to_pos: HashMap<SyntaxId, Position>,
+    replace_positions: Vec<Position>,
+}
 
-impl Visitor for RenameLocalVisitor {}
+impl Visitor for RenameLocalVisitor {
+    fn visit_symbol(&mut self, symbol: &Symbol) {
+        let Some(sym_def_pos) = self.id_to_pos.get(&symbol.id) else {
+            return;
+        };
+        if *sym_def_pos != self.definition_pos {
+            return;
+        }
+
+        self.replace_positions.push(symbol.position.clone());
+    }
+}

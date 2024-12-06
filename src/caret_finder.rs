7a0aff4d9e84827484112c660cc7b97bcb22ea3c
Wilfred Hughes <me@wilfred.me.uk>
2024-12-06 19:59:28 +0000

    tests: Define a caret finder that does ranges rather than single points

--- src/caret_finder.rs
+++ src/caret_finder.rs
@@ -15,3 +15,23 @@ pub(crate) fn find_caret_offset(src: &str) -> Option<usize> {
     let prev_line_start = src[..prev_line_end].rfind('\n').unwrap_or(0);
     Some(prev_line_start + column)
 }
+
+/// Given a string that contains `// ^^^`, return the byte offset of
+/// the character indicated by the first ^ and the last ^ (i.e. the
+/// characters on the line above).
+pub(crate) fn find_caret_region(src: &str) -> Option<(usize, usize)> {
+    let re = Regex::new(r"// *(\^+)").unwrap();
+
+    let comment_caps = re.captures(src)?;
+
+    let last_caret_offset = comment_caps.get(0).unwrap().end();
+    let first_caret_offset = comment_caps.get(1).unwrap().start();
+
+    let prev_line_end = src[..first_caret_offset].rfind('\n')?;
+    let prev_line_start = src[..prev_line_end].rfind('\n').unwrap_or(0);
+
+    let first_char_offset = prev_line_start + (first_caret_offset - prev_line_end);
+    let last_char_offset = prev_line_start + (last_caret_offset - prev_line_end);
+
+    Some((first_char_offset, last_char_offset))
+}

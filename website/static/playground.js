43a56ee2fff4e1c764b5dddefc0d7fb854a49fa3
Wilfred Hughes <me@wilfred.me.uk>
2025-11-21 11:25:24 +0000

    Basic snippet output rendering when clicking run button

--- website/static/playground.js
+++ website/static/playground.js
@@ -1,24 +1,32 @@
-var _a;
-console.log("hello world");
-function evalSnippet(src) {
+var _a, _b;
+function evalSnippet(src, snippetDiv) {
+    // TODO: why doesn't typescript know about the hidden field on Element?
+    var snippetElement = snippetDiv;
+    snippetElement.hidden = false;
     fetch("/good.json")
         .then(function (response) { return response.text(); })
         .then(function (responseText) {
         var evalResult = JSON.parse(responseText);
+        snippetDiv.innerHTML = responseText;
         console.log("Evaluating: " + src);
         console.log(evalResult);
     });
 }
 var _loop_1 = function (button) {
     console.log(button);
-    var codeNode = (_a = button === null || button === void 0 ? void 0 : button.parentNode) === null || _a === void 0 ? void 0 : _a.nextSibling;
+    var snippetDiv = (_a = button === null || button === void 0 ? void 0 : button.parentNode) === null || _a === void 0 ? void 0 : _a.parentNode;
+    var codeNode = (_b = button === null || button === void 0 ? void 0 : button.parentNode) === null || _b === void 0 ? void 0 : _b.nextSibling;
     var src = (codeNode === null || codeNode === void 0 ? void 0 : codeNode.textContent) || "";
     button.addEventListener("click", function (_e) {
-        evalSnippet(src);
+        if (snippetDiv) {
+            var outputDiv = snippetDiv.querySelector(".snippet-output");
+            if (outputDiv) {
+                evalSnippet(src, outputDiv);
+            }
+        }
     });
 };
-for (var _i = 0, _b = document.querySelectorAll(".run-snippet"); _i < _b.length; _i++) {
-    var button = _b[_i];
+for (var _i = 0, _c = document.querySelectorAll(".run-snippet"); _i < _c.length; _i++) {
+    var button = _c[_i];
     _loop_1(button);
 }
-console.log("Ready");
--- website/static/playground.ts
+++ website/static/playground.ts
@@ -1,10 +1,14 @@
-console.log("hello world");
+function evalSnippet(src: string, snippetDiv: Element) {
+  // TODO: why doesn't typescript know about the hidden field on Element?
+  let snippetElement = snippetDiv as Element & { hidden: boolean };
+  snippetElement.hidden = false;
 
-function evalSnippet(src: string) {
   fetch("/good.json")
     .then((response) => response.text())
     .then((responseText) => {
       let evalResult = JSON.parse(responseText);
+
+      snippetDiv.innerHTML = responseText;
 
       console.log("Evaluating: " + src);
       console.log(evalResult);
@@ -14,12 +18,16 @@ function evalSnippet(src: string) {
 for (const button of document.querySelectorAll(".run-snippet")) {
   console.log(button);
 
+  let snippetDiv = button?.parentNode?.parentNode;
   let codeNode = button?.parentNode?.nextSibling;
   let src = codeNode?.textContent || "";
 
   button.addEventListener("click", (_e) => {
-    evalSnippet(src);
+    if (snippetDiv) {
+      let outputDiv = snippetDiv.querySelector(".snippet-output");
+      if (outputDiv) {
+        evalSnippet(src, outputDiv);
+      }
+    }
   });
 }
-
-console.log("Ready");

bfb1ba182e917398b8811c86c840c5ab36e65bc0
Wilfred Hughes <me@wilfred.me.uk>
2025-11-22 00:08:54 +0000

    Add a /run endpoint

--- playground/index.js
+++ playground/index.js
@@ -1,10 +1,35 @@
 const http = require('http');
+const { exec } = require('child_process');
+const url = require('url');
 
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('hello world');
+  const parsedUrl = url.parse(req.url, true);
+
+  if (parsedUrl.pathname === '/run') {
+    exec('ls /', (error, stdout, stderr) => {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+
+      if (error) {
+        res.end(JSON.stringify({
+          success: false,
+          error: error.message,
+          stderr: stderr
+        }));
+        return;
+      }
+
+      const files = stdout.trim().split('\n');
+      res.end(JSON.stringify({
+        success: true,
+        files: files
+      }));
+    });
+  } else {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('hello world');
+  }
 });
 
 server.listen(PORT, () => {

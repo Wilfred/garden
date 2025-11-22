aeb0044d2293fdb7c461230f276057866281583b
Wilfred Hughes <me@wilfred.me.uk>
2025-11-22 00:11:55 +0000

    Prefer using express

--- playground/index.js
+++ playground/index.js
@@ -1,37 +1,31 @@
-const http = require('http');
+const express = require('express');
 const { exec } = require('child_process');
-const url = require('url');
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  const parsedUrl = url.parse(req.url, true);
-
-  if (parsedUrl.pathname === '/run') {
-    exec('ls /', (error, stdout, stderr) => {
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-
-      if (error) {
-        res.end(JSON.stringify({
-          success: false,
-          error: error.message,
-          stderr: stderr
-        }));
-        return;
-      }
-
-      const files = stdout.trim().split('\n');
-      res.end(JSON.stringify({
-        success: true,
-        files: files
-      }));
-    });
-  } else {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('hello world');
-  }
+app.get('/', (req, res) => {
+  res.send('hello world');
 });
 
-server.listen(PORT, () => {
+app.get('/run', (req, res) => {
+  exec('ls /', (error, stdout, stderr) => {
+    if (error) {
+      return res.json({
+        success: false,
+        error: error.message,
+        stderr: stderr
+      });
+    }
+
+    const files = stdout.trim().split('\n');
+    res.json({
+      success: true,
+      files: files
+    });
+  });
+});
+
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
